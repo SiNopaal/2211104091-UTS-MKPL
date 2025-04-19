2d1d6e19cf472f107373c05fb6e8048b88fa3677
SiNopaal <naufalmaulana806@gmail.com>
2025-04-19 21:26:13 +0700

    Refactor Long Method in TaxFunction.java by extracting smaller methods

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -2,27 +2,47 @@ package lib;
 
 public class TaxFunction {
 
-    /**
-     * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-     */
-    public static int calculateTax(EmployeeIncomeData data) {
-        if (data.numberOfMonthWorking > 12) {
-            System.err.println("More than 12 month working per year");
+    private static final int PTKP_SINGLE = 54000000;
+    private static final int PTKP_MARRIED = 4500000;
+    private static final int PTKP_PER_CHILD = 1500000;
+    private static final int MAX_CHILDREN = 3;
+
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int monthsWorked, int deductible, boolean isMarried, int numberOfChildren) {
+        if (monthsWorked > 12) {
+            System.err.println("More than 12 months worked in a year");
+            monthsWorked = 12; // optional safety
         }
 
-        int numberOfChildren = Math.min(data.numberOfChildren, 3);
-
-        // Hitung penghasilan tidak kena pajak
-        int nonTaxableIncome = 54000000;
-        if (data.isMarried) {
-            nonTaxableIncome += 4500000;
+        if (numberOfChildren > MAX_CHILDREN) {
+            numberOfChildren = MAX_CHILDREN;
         }
-        nonTaxableIncome += numberOfChildren * 1500000;
 
-        int yearlyIncome = (data.monthlySalary + data.otherMonthlyIncome) * data.numberOfMonthWorking;
-        int taxableIncome = yearlyIncome - data.deductible - nonTaxableIncome;
+        int annualIncome = calculateAnnualIncome(monthlySalary, otherMonthlyIncome, monthsWorked);
+        int ptkp = calculatePTKP(isMarried, numberOfChildren);
+        int taxableIncome = calculateTaxableIncome(annualIncome, deductible, ptkp);
 
-        int tax = (int) Math.round(0.05 * Math.max(taxableIncome, 0));
-        return tax;
+        return calculateFinalTax(taxableIncome);
+    }
+
+    private static int calculateAnnualIncome(int salary, int otherIncome, int monthsWorked) {
+        return (salary + otherIncome) * monthsWorked;
+    }
+
+    private static int calculatePTKP(boolean isMarried, int numberOfChildren) {
+        int ptkp = PTKP_SINGLE;
+        if (isMarried) {
+            ptkp += PTKP_MARRIED;
+            ptkp += PTKP_PER_CHILD * numberOfChildren;
+        }
+        return ptkp;
+    }
+
+    private static int calculateTaxableIncome(int annualIncome, int deductible, int ptkp) {
+        return annualIncome - deductible - ptkp;
+    }
+
+    private static int calculateFinalTax(int taxableIncome) {
+        if (taxableIncome <= 0) return 0;
+        return (int) Math.round(0.05 * taxableIncome);
     }
 }
