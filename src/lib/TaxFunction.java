c12cb1052e404ea7192b3bb04fdd51e3937c005e
SiNopaal <naufalmaulana806@gmail.com>
2025-04-19 21:19:40 +0700

    Refactor TaxFunction to use EmployeeIncomeData object instead of long parameter list

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -6,17 +6,13 @@ public class TaxFunction {
      * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
      */
     public static int calculateTax(EmployeeIncomeData data) {
-        int tax;
-
         if (data.numberOfMonthWorking > 12) {
             System.err.println("More than 12 month working per year");
         }
 
-        int numberOfChildren = data.numberOfChildren;
-        if (numberOfChildren > 3) {
-            numberOfChildren = 3;
-        }
+        int numberOfChildren = Math.min(data.numberOfChildren, 3);
 
+        // Hitung penghasilan tidak kena pajak
         int nonTaxableIncome = 54000000;
         if (data.isMarried) {
             nonTaxableIncome += 4500000;
@@ -24,8 +20,9 @@ public class TaxFunction {
         nonTaxableIncome += numberOfChildren * 1500000;
 
         int yearlyIncome = (data.monthlySalary + data.otherMonthlyIncome) * data.numberOfMonthWorking;
-        tax = (int) Math.round(0.05 * (yearlyIncome - data.deductible - nonTaxableIncome));
+        int taxableIncome = yearlyIncome - data.deductible - nonTaxableIncome;
 
-        return Math.max(tax, 0);
+        int tax = (int) Math.round(0.05 * Math.max(taxableIncome, 0));
+        return tax;
     }
 }
